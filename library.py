870bb63646c06de666a67ebf2fc1e1f7cc612152
Mikhail <sfinkssupercat@gmail.com>
2024-12-04 22:51:33 +0300

    changed id func, changed status input, changed year input, added comments

--- library.py
+++ library.py
@@ -1,4 +1,5 @@
 import json
+from datetime import date
 
 # Создаем класс книги
 class Book:
@@ -14,14 +15,16 @@ class Book:
             return f"{self.book_id}: {self.title} - {self.author} ({self.year}) - {self.status}"
 
 
-# Функции для загрузки и сохранения данных в JSON:
+# Функции для загрузки данных из JSON файла:
 def load_books(filename='books.json'):
+    # Если файл существует получаем список книг, если нет, возвращаем пустой список
     try:
         with open(filename, 'r', encoding='utf-8') as f:
             return [Book(**book) for book in json.load(f)]
     except FileNotFoundError:
         return []
 
+# Сохраняем книгу в JSON файл
 def save_books(books, filename='books.json'):
     with open(filename, 'w', encoding='utf-8') as f:
         # Сохраняем книгу из класса в JSON используя ensure_ascii=False для сохранения в удобочитаемом формате
@@ -31,7 +34,10 @@ def save_books(books, filename='books.json'):
 
 # Добавление новой книги в библиотеку
 def add_book(books, title, author, year):
-    book_id = len(books) + 1  
+    # Для нескольких файлов проще всего было бы сделать так:
+    # book_id = len(books) + 1 
+    # Но в случае удаления книги из середины библиотеки новая книга допишется с уже существующим ID
+    book_id = id(title) 
     new_book = Book(book_id, title, author, year)
     books.append(new_book)
     save_books(books)
@@ -40,6 +46,7 @@ def add_book(books, title, author, year):
 # Удаление книги
 def remove_book(books, book_id):
     for book in books:
+        # Если искомый id существует - книга удаляется
         if book.book_id == book_id:
             books.remove(book)
             save_books(books)
@@ -50,6 +57,7 @@ def remove_book(books, book_id):
 
 # Поиск
 def search_books(books, query):
+    # Если в списке есть книга удовлетворяющая запрос пользователя, то она добавляется в список найденых книг
     results = [book for book in books if query.lower() in book.title.lower() or 
             query.lower() in book.author.lower() or 
             str(book.year) == query]
@@ -79,7 +87,9 @@ def change_status(books, book_id, status):
 
 # Взаимодействие с пользователем
 def main():
+    # Получаем список сохраненных книг
     books = load_books()
+    # Меню
     while True:
         print('\n1. Добавить книгу')
         print('2. Удалить книгу')
@@ -88,24 +98,27 @@ def main():
         print('5. Изменить статус книги')
         print('0. Выйти\n')
         choice = input('Выберите действие: ')
-
-        if choice == '1':
+        
+        if choice == '1':           
+            title = input('Введите название книги: ')
+            author = input('Введите автора книги: ')
             try:
-                title = input('Введите название книги: ')
-                author = input('Введите автора книги: ')
-                year = int(input('Введите год издания: '))
-
-                add_book(books, title, author, year)
+                year = int(input('Введите год издания: '))                
             except(ValueError):
                 print('Неверный ввод.')
+            else:
+                today = date.today()
+                if 0 < year <= int(today.strftime('%Y')):
+                    add_book(books, title, author, year)
+                else:
+                    print('Дата указана неверно.')
             
-
         elif choice == '2':
             book_id = int(input('Введите ID книги для удаления: '))
             remove_book(books, book_id)
 
         elif choice == '3':
-            query = input('Введите название, автора или год книги для поиска: ')
+            query = input('Введите название, автора или год издания книги для поиска: ')
             results = search_books(books, query)
             if results:
                 for book in results:
@@ -117,9 +130,21 @@ def main():
             display_books(books)
 
         elif choice == '5':
-            book_id = int(input('Введите ID книги: '))
-            status = input('Введите новый статус (в наличии/выдана): ')
-            change_status(books, book_id, status)
+            try:
+                book_id = int(input('Введите ID книги: '))
+            except(ValueError):
+                print('Неверный ввод.')
+            else:    
+                new_status = input('Введите новый статус (1 - в наличии; 2 - выдана): ')
+                if new_status == '1':
+                    status = 'в наличии'
+                    change_status(books, book_id, status)
+                elif new_status == '2':
+                    status = 'выдана'
+                    change_status(books, book_id, status)
+                else:
+                    print('Неверный ввод.')
+                
 
         elif choice == '0':
             print('\nДо свидания')
